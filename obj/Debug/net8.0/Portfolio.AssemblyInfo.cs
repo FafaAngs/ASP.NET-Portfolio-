b0073a42ef69165a8e27fbc84db42c965461ede4
Angs <118002169+FafaAngs@users.noreply.github.com>
2025-04-29 20:20:44 +0800

    prepare

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31a7f4413d79fa255f046229f56efc9781c49b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
